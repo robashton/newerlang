7c096bebe634d4ca2774deebe70652721b530ac0
Rob Ashton <robashton@codeofrob.com>
2013-12-14 19:17:06 +0000

    Playing more

--- src/helloworld_srv.erl
+++ src/helloworld_srv.erl
@@ -6,7 +6,7 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0]).
+-export([start_link/0, say_/0 stop/0]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -14,6 +14,17 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
+
+
+%% -- My functions
+%%
+
+say_hello() ->
+  gen_server:call(?MODULE, hello).
+
+stop() ->
+  gen_server:cast(?MODULE, stop).
+
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
@@ -30,8 +41,16 @@ init(Args) ->
     {ok, Args}.
 
 handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+    {reply, ok, State};
+handle_call(hello, _From, State) ->
+  io:format("Hello world!!", []),
+  {reply, ok, State};
+handle_call(_Request, _From, State) ->
+  Reply = ok,
+  {reply, Reply, State}.
 
+handle_cast(stop, State) ->
+  {stop, normal, State};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
